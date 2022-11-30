45fd7db01d0226945f66fad2b2a044e26e501c6d
Angelica <mmaderacovo@gmail.com>
2022-11-30 11:23:58 -0500

    Swith case Ejercicio

--- src/Switch.java
+++ src/Switch.java
@@ -1,5 +1,26 @@
 public class Switch {
     public static void main(String[] args) {
 
+        String colorSelected = "1 ";
+
+        switch (colorSelected){
+            case "Light":
+                System.out.println("Seleccionaste Light Mode");
+                break;
+            case "Night":
+                System.out.println("Seleccionaste Night Mode");
+                break;
+            case "Blue Dark":
+                System.out.println("Seleccionaste Blue Dark Mode");
+                break;
+            case "Dark":
+                System.out.println("Seleccionaste Dark Mode");
+                break;
+            default:
+                System.out.println("Selecciona una opcion");
+
+        }
+
+
     }
 }
