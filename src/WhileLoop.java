2aee188c9950f127cd9bb23a0786c651786575cb
Angelica <mmaderacovo@gmail.com>
2022-12-06 10:59:34 -0500

    Uso de While Java

---
+++ src/WhileLoop.java
@@ -0,0 +1,20 @@
+public class WhileLoop {
+    static boolean isTurnOnLight = true;
+    public static void main(String[] args) {
+
+        turnOfflight();
+        int i = 1;
+
+        while (isTurnOnLight && i<=10){
+            printSOS();
+            i++;
+        }
+    }
+    public static void printSOS (){
+        System.out.println(". . . _ _ _ . . .");
+    }
+    public static boolean turnOfflight(){
+        isTurnOnLight = (isTurnOnLight)?false:true;
+        return isTurnOnLight;
+    }
+}
