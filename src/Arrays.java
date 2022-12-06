840f071e547354704a87154a36d2bf9da9a20725
Angelica <mmaderacovo@gmail.com>
2022-12-06 17:04:43 -0500

    Uso de For Anidado Java

--- src/Arrays.java
+++ src/Arrays.java
@@ -1,21 +1,29 @@
 public class Arrays {
     public static void main(String[] args) {
-        String[] androidVersion = new String[17];
+        String[] androidVersions = new String[17];
         String days[] = new String[7];
         String [][] cities = new String[4][2];
 
         int[][][] numbers = new int[2][2][2];
         int [][][][] numbers4 = new int[2][2][2][2];
 
-        androidVersion[0]= "Apple Pie";
-        androidVersion[1]= "Bannana Bread";
-        androidVersion[2]= "Cupcake";
-        androidVersion[3]= "Donut";
+        androidVersions[0]= "Apple Pie";
+        androidVersions[1]= "Bannana Bread";
+        androidVersions[2]= "Cupcake";
+        androidVersions[3]= "Donut";
 
-        System.out.println(androidVersion[0]);
+        /*for (int i =0;i <androidVersion.length; i++){
+            System.out.println(androidVersion[i]);
+        }*/
+
+        for (String androidVersion:androidVersions) {
+            System.out.println(androidVersion);
+        }
+
+        /*System.out.println(androidVersion[0]);
         System.out.println(androidVersion[1]);
         System.out.println(androidVersion[2]);
-        System.out.println(androidVersion[3]);
+        System.out.println(androidVersion[3]);*/
 
         System.out.println();
         System.out.println();
@@ -29,14 +37,42 @@ public class Arrays {
         cities[3][0] = "Mexico";
         cities[3][1] = "CDMX";
 
-        System.out.println(cities[0][0]);
+        /*System.out.println(cities[0][0]);
         System.out.println(cities[0][1]);
         System.out.println(cities[1][0]);
         System.out.println(cities[1][1]);
         System.out.println(cities[2][0]);
         System.out.println(cities[2][1]);
         System.out.println(cities[3][0]);
-        System.out.println(cities[3][1]);
+        System.out.println(cities[3][1]);*/
 
+        /*for (int i = 0; i < cities.length; i++) {
+            for (int j = 0; j < cities[1].length; j++) {
+                System.out.println(cities[i][j]);
+            }
+        }*/
+
+        for (String[] pair:cities) {
+            for (String name:pair) {
+                System.out.println(name);
+            }
+        }
+
+        String[][][][] animals = new String[2][3][2][2];
+        animals[1][0][0][1] = "Monkey";
+
+        System.out.println();
+        System.out.println();
+
+
+        for (int i = 0; i <= 1; i++) {
+            for (int j = 0; j <= 0; j++) {
+                for (int k = 0; k <= 0; k++) {
+                    for (int l = 0; l <= 1; l++) {
+                        System.out.println(animals[i][j][k][l]);
+                    }
+                }
+            }
+        }
     }
 }
