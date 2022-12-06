29c020974df640569232eb8ec4ec3f1a97afe2f8
Angelica <mmaderacovo@gmail.com>
2022-12-06 10:13:13 -0500

    Funcion Convert To Dollar Java

--- src/Funciones.java
+++ src/Funciones.java
@@ -1,10 +1,14 @@
 public class Funciones {
     public static void main(String[] args) {
         double y = 3;
+
         //Area del un circulo
         //pi * r2
         double areaCircle = circleArea(y);
         System.out.println("Area del circulo= " + areaCircle);
+
+        //Uso de la funcion
+        System.out.println("Dollar= "+ convertToDollar(20,"COP"));
 
         //Area de una esfera
         //4*PI*r2
@@ -15,6 +19,7 @@ public class Funciones {
         //(4/3)*PI*r3
         double sphereVolume = sphereVolume(y);
         System.out.println("Volumen de la esfera = " + sphereVolume);
+
     }
 
     public static double circleArea(double r){
@@ -27,5 +32,17 @@ public class Funciones {
 
     public static double sphereVolume (double r){
         return (4/3)*Math.PI*Math.pow(r,3);
+    }
+
+    public static double convertToDollar (double quantity, String currency){
+        switch (currency){
+            case "MXN":
+                quantity = quantity * 0.051;
+                break;
+            case "COP":
+                quantity = quantity * 0.00021;
+                break;
+        }
+        return quantity;
     }
 }
