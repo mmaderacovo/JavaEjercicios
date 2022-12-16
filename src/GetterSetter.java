3ff896b7b363dd7d6895161d2448038ffd68acd8
Angelica <mmaderacovo@gmail.com>
2022-12-16 14:59:44 -0500

    Uso de Getter y Setteren java

---
+++ src/GetterSetter.java
@@ -0,0 +1,8 @@
+public class GetterSetter {
+    public static void main(String[] args) {
+
+        Main main= new Main();
+        main.setId(33);//Asignar valor
+        main.getId();//Obtener valor
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
+    private Integer id;
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
     }
 }
